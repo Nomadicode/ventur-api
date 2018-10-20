84e842b3466b6091fe38bdcf09ca71e7dda64c95
= <fraghealer@nomadicode.com>
2018-10-20 17:50:46 -0600

    Adding corsheaders to api

--- api/settings.py
+++ api/settings.py
@@ -49,6 +49,7 @@ class Base(Configuration):
     ]
 
     MIDDLEWARE = [
+        'corsheaders.middleware.CorsMiddleware',
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -57,6 +58,8 @@ class Base(Configuration):
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ]
+    
+    CORS_ORIGIN_ALLOW_ALL = True
 
     ROOT_URLCONF = 'api.urls'
 
