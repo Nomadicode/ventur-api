898fa5bcd6e9961ea3e7e359ce9845debc8c5e7f
Jamahl Middleton <dev@nomadicode.com>
2018-10-22 15:25:31 +0000

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -204,7 +204,7 @@ class Base(Configuration):
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.1/howto/static-files/
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/static/'
 
 
