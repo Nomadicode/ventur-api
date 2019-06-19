a8915e1edf46cdb023ddcdfd848561e7b705ee24
Jamahl Middleton <jamahl@protonmail.com>
2019-06-19 13:02:33 -0600

    Attempting a fix to CORS issue

--- api/settings.py
+++ api/settings.py
@@ -76,13 +76,16 @@ class Base(Configuration):
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
+        'corsheaders.middleware.CorsPostCsrfMiddleware'
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ]
     
     CORS_ORIGIN_ALLOW_ALL = True
+
+    CORS
 
     ROOT_URLCONF = 'api.urls'
 
