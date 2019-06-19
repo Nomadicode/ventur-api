73f72f6699c9e9504f3215d92e7d3d60991055ca
Jamahl Middleton <jamahl@protonmail.com>
2019-06-19 17:48:46 -0600

    fixing cors

--- api/settings.py
+++ api/settings.py
@@ -76,6 +76,7 @@ class Base(Configuration):
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
+        'api.middleware.DisableCsrfMiddleware.DisableCSRF'
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
