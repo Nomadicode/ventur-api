0e782d2d7ddbd087a83ec5837dc878ce386b1896
Jamahl Middleton <jamahl@protonmail.com>
2019-06-19 18:50:53 -0600

    ADding middleware

--- api/middleware/DisableCsrfMiddleware.py
+++ api/middleware/DisableCsrfMiddleware.py
@@ -1,6 +1,8 @@
-from django.utils.deprecation import MiddlewareMixin
+class DisableCSRF(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-
-class DisableCSRF(MiddlewareMixin):
-    def process_request(self, request):
+    def __call__(self, request):
         setattr(request, '_dont_enforce_csrf_checks', True)
+        response = self.get_response(request)
+        return response
